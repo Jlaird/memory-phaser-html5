c156263184efd55d7b60b11c9728bd577cb6ec0c
Jeremy Laird <Jeremy.Laird@cygnus.com>
2021-08-05 11:15:54 +0000

    can now open boxes

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,5 +1,12 @@
 import Phaser from 'phaser'
 
+// 👇 level variable as 2D array of numbers
+const level = [
+	[1, 0, 3],
+	[2, 4, 1],
+	[3, 4, 2]
+]
+
 export default class Game extends Phaser.Scene
 {
 
@@ -11,6 +18,12 @@ export default class Game extends Phaser.Scene
 
 	/** @type {Phaser.Physics.Arcade.StaticGroup} */
 	boxGroup
+
+	/** @type {Phaser.Physics.Arcade.Sprite} */
+	activeBox
+
+	/** @type {Phaser.GameObjects.Group} */
+	itemsGroup
 
 	constructor()
 	{
@@ -34,7 +47,51 @@ export default class Game extends Phaser.Scene
         
 		this.createBoxes()
 
-		this.physics.add.collider(this.player, this.boxGroup)
+		this.physics.add.collider(this.player, this.boxGroup, this.handlePlayerBoxCollide, undefined, this)
+
+		this.itemsGroup = this.add.group()
+	}
+
+	updateActiveBox()
+	{
+		if (!this.activeBox)
+		{
+			return
+		}
+
+		// get the distance here 👇
+		const distance = Phaser.Math.Distance.Between(
+			this.player.x, this.player.y,
+			this.activeBox.x, this.activeBox.y
+		)
+
+		if (distance < 64) // 👈 do nothing if still near
+		{
+			return
+		}
+
+		// return to using frame 10 when too far
+		this.activeBox.setFrame(10)
+
+		// and make activeBox undefined
+		this.activeBox = undefined
+	}
+
+	/**
+	 * 
+	 * @param {Phaser.Physics.Arcade.Sprite} player 
+	 * @param {Phaser.Physics.Arcade.Sprite} box
+	 */
+	handlePlayerBoxCollide(player, box)
+	{
+		if (this.activeBox)
+		{
+			return
+		}
+
+		this.activeBox = box
+
+		this.activeBox.setFrame(9)
 	}
 
 	createBoxes()
@@ -43,14 +100,15 @@ export default class Game extends Phaser.Scene
 
 		let xPer = 0.25
 		let y = 150
-		for (let row = 0; row < 3; ++row)
+		for (let row = 0; row < level.length; ++row)
 		{
-			for (let col = 0; col < 3; ++col)
+			for (let col = 0; col < level[row].length; ++col)
 			{
 				/** @type {Phaser.Physics.Arcade.Sprite} */
 				const box = this.boxGroup.get(width * xPer, y, 'sokoban', 10)
 				box.setSize(64, 32)
 					.setOffset(0, 32)
+					.setData('itemType', level[row][col])
 
 				xPer += 0.25
 			}
@@ -60,47 +118,132 @@ export default class Game extends Phaser.Scene
 		}
 	}
 
-	update()
+	/**
+	 * 
+	 * @param {Phaser.Physics.Arcade.Sprite} box 
+	 */
+	openBox(box)
 	{
-	const speed = 200
+		if (!box)
+		{
+			return
+		}
 
-	if (this.cursors.left.isDown)
-	{
-		this.player.setVelocity(-speed, 0)
-		this.player.play('left-walk', true)
+		const itemType = box.getData('itemType')
+			
+		/** @type {Phaser.GameObjects.Sprite} */
+		let item
+
+		switch (itemType)
+		{
+			case 0:
+				item = this.itemsGroup.get(box.x, box.y)
+				item.setTexture('bear')
+				break
+
+			case 1:
+				item = this.itemsGroup.get(box.x, box.y)
+				item.setTexture('chicken')
+				break
+
+			case 2:
+				item = this.itemsGroup.get(box.x, box.y)
+				item.setTexture('duck')
+				break
+
+			case 3:
+				item = this.itemsGroup.get(box.x, box.y)
+				item.setTexture('parrot')
+				break
+
+			case 4:
+				item = this.itemsGroup.get(box.x, box.y)
+				item.setTexture('penguin')
+				break
+		}
+
+		box.setData('opened', true)
+
+		item.scale = 0
+		item.alpha = 0
+	
+		this.tweens.add({
+			targets: item,
+			y: '-=50',
+			alpha: 1,
+			scale: 1,
+			duration: 500
+		})
 	}
-	else if (this.cursors.right.isDown)
+
+	updatePlayer()
 	{
-		this.player.setVelocity(speed, 0)
-		this.player.play('right-walk', true)
-	}
-	else if (this.cursors.up.isDown)
-	{
-		this.player.setVelocity(0, -speed)
-		this.player.play('up-walk', true)
-	}
-	else if (this.cursors.down.isDown)
-	{
-		this.player.setVelocity(0, speed)
-		this.player.play('down-walk', true)
-	}
-	else
-	{
-		this.player.setVelocity(0, 0)
-		if (this.player.anims.currentAnim?.key) {
-			const key = this.player.anims.currentAnim.key
-			const parts = key.split('-')
-			const direction = parts[0]
-			this.player.play(`${direction}-idle`)
+		const speed = 200
+
+		if (this.cursors.left.isDown)
+		{
+			this.player.setVelocity(-speed, 0)
+			this.player.play('left-walk', true)
+		}
+		else if (this.cursors.right.isDown)
+		{
+			this.player.setVelocity(speed, 0)
+			this.player.play('right-walk', true)
+		}
+		else if (this.cursors.up.isDown)
+		{
+			this.player.setVelocity(0, -speed)
+			this.player.play('up-walk', true)
+		}
+		else if (this.cursors.down.isDown)
+		{
+			this.player.setVelocity(0, speed)
+			this.player.play('down-walk', true)
+		}
+		else
+		{
+			this.player.setVelocity(0, 0)
+			if (this.player.anims.currentAnim?.key) {
+				const key = this.player.anims.currentAnim.key
+				const parts = key.split('-')
+				const direction = parts[0]
+				this.player.play(`${direction}-idle`)
+			}
+		}
+
+		const spaceJustPressed = Phaser.Input.Keyboard.JustUp(this.cursors.space)
+		if (spaceJustPressed && this.activeBox)
+		{
+			this.openBox(this.activeBox)
+
+			this.activeBox.setFrame(10)
+			this.activeBox = undefined
 		}
 	}
 
-	this.children.each(c => {
-		/** @type {Phaser.Physics.Arcade.Sprite} */
-		// @ts-ignore
-		const child = c
+	update()
+	{
+	
+		this.updatePlayer()
 
-		child.setDepth(child.y)
-	})
-}
+		const spaceJustPressed = Phaser.Input.Keyboard.JustUp(this.cursors.space)
+		if (spaceJustPressed && this.activeBox)
+		{
+			this.openBox(this.activeBox)
+	
+			// reset box after opened
+			this.activeBox.setFrame(10)
+			this.activeBox = undefined
+		}
+
+		this.updateActiveBox()
+
+		this.children.each(c => {
+			/** @type {Phaser.Physics.Arcade.Sprite} */
+			// @ts-ignore
+			const child = c
+
+			child.setDepth(child.y)
+		})
+	}
 }
